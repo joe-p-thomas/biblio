e01f9465eb5e8473b46bbc52b58abf09c1bd3137
Joe Thomas <joe.p.thomas92@gmail.com>
2017-04-27 08:49:26 -0700

    add sort functionality to book index

--- frontend/components/books/book_index.jsx
+++ frontend/components/books/book_index.jsx
@@ -5,7 +5,12 @@ import BookIndexItem from './book_index_item';
 class BookIndex extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      sortBy: "author"
+    };
     this.handleClick = this.handleClick.bind(this);
+    this.handleSortClick = this.handleSortClick.bind(this);
+    this.sortBy = this.sortBy.bind(this);
   }
 
   handleClick(e) {
@@ -33,8 +38,68 @@ class BookIndex extends React.Component {
     e.stopPropagation();
   }
 
+  handleSortClick(property) {
+    this.setState({sortBy: property});
+  }
+
+  sortBy(property) {
+    let sortOrder = 1;
+    if (property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+    }
+    if (property === "author") {
+      return this.sortByAuthor(sortOrder);
+    } else if (property === "title") {
+      return this.sortByTitle(sortOrder);
+    }
+  }
+
+  sortByAuthor(sortOrder) {
+    return function (a,b) {
+        let aLastName = a.author.split(" ").pop();
+        let bLastName = b.author.split(" ").pop();
+
+        let result;
+        if (aLastName < bLastName) {
+          result = -1;
+        } else if (aLastName > bLastName) {
+          result = 1;
+        } else {
+          result = 0;
+        }
+        return result * sortOrder;
+    };
+  }
+
+  sortByTitle(sortOrder) {
+    function trimTitle(title) {
+      if (title.substring(0,2) === "A ") {
+        title = title.slice(2);
+      } else if (title.substring(0,4) === "The ") {
+        title = title.slice(4);
+      }
+      return title;
+    }
+
+    return function (a,b) {
+        let aTitle = trimTitle(a.title);
+        let bTitle = trimTitle(b.title);
+
+        let result;
+        if (aTitle < bTitle) {
+          result = -1;
+        } else if (aTitle > bTitle) {
+          result = 1;
+        } else {
+          result = 0;
+        }
+        return result * sortOrder;
+    };
+  }
+
   render() {
-    const books = this.props.books.map((book, idx) => (
+    const books = this.props.books.sort(this.sortBy(this.state.sortBy)).map((book, idx) => (
       <BookIndexItem key={book.id}
                      book={book}
                      bookshelf={this.props.bookshelf}
@@ -43,6 +108,10 @@ class BookIndex extends React.Component {
     return(
       <div className='book-index'>
         <h2>{this.props.title}</h2>
+        <ul>
+          <li onClick={() => this.handleSortClick("author")}>author</li>
+          <li onClick={() => this.handleSortClick("title")}>title</li>
+        </ul>
         <ul className='book-list'
             onClick={this.handleClick}>
           {books}
